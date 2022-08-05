daee57a28e7925e2a66bb4a0eebfde6a878d1900
unknown <daniil.tsiganov@gmail.com>
2022-08-06 01:24:14 +0300

    подписки

--- backend/api/views.py
+++ backend/api/views.py
@@ -27,9 +27,9 @@ class UserViewset(UserViewSet):
 
     pagination_class = CustomPagination
 
-    @action(detail=True, permission_classes=(IsAuthenticated,))
+    @action(detail=True, permission_classes=[IsAuthenticated])
     def subscribe(self, request, id=None):
-        """Создание подписки."""
+        """Подписка на пользователей."""
 
         user = request.user
         author = get_object_or_404(User, id=id)
@@ -51,34 +51,33 @@ class UserViewset(UserViewSet):
 
     @subscribe.mapping.delete
     def del_subscribe(self, request, id=None):
-        """Отписаться."""
+        """Отписка."""
         user = request.user
         author = get_object_or_404(User, id=id)
         if user == author:
-            return Response(
-                {"errors": "Вы не можете отписываться от самого себя"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+            return Response({
+                "errors": "Вы не можете отписываться от самого себя"
+            }, status=status.HTTP_400_BAD_REQUEST)
         follow = Subscribe.objects.filter(user=user, author=author)
         if follow.exists():
             follow.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(
-            {"errors": "Вы уже отписались"},
-            status=status.HTTP_400_BAD_REQUEST
-        )
 
-    @action(detail=False, permission_classes=(IsAuthenticated,))
+        return Response({
+            "errors": "Вы уже отписались"
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, permission_classes=[IsAuthenticated])
     def subscriptions(self, request):
-        """Получение данных о подписках."""
-
+        """Список подписок пользователя."""
         user = request.user
         queryset = Subscribe.objects.filter(user=user)
         pages = self.paginate_queryset(queryset)
         serializer = SubscribeSerializer(
             pages,
             many=True,
-            context={"request": request})
+            context={"request": request}
+        )
         return self.get_paginated_response(serializer.data)
 
 
